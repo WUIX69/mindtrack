a8847b96441cd7a2b159762d8886c5cc8e723b0e
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-10-23 12:25:54 +0800

    Update configuration to change sub_folder and database name to 'mindtrack_tan' for environment consistency

--- core/config.php
+++ core/config.php
@@ -5,17 +5,17 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 $config = [
-    'sub_folder' => 'mindtrack',
+    'sub_folder' => 'mindtrack_tan',
     'root_path' => __DIR__ . '/../',  // Project root directory
     'uri_path' => $_SERVER['REQUEST_URI'],
     'app' => [
-        'base_url' => $_ENV['APP_URL'] ?? 'http://localhost/mindtrack',
+        'base_url' => $_ENV['APP_URL'] ?? 'http://localhost/mindtrack_tan',
         'assets_url' => '/assets',
         'name' => $_ENV['APP_NAME'] ?? 'MindTrack',
     ],
     'db' => [
         'host' => $_ENV['DB_HOST'] ?? 'localhost',
-        'name' => $_ENV['DB_DATABASE'] ?? 'mindtrack',
+        'name' => $_ENV['DB_DATABASE'] ?? 'mindtrack_tan',
         'port' => $_ENV['DB_PORT'] ?? '3306',
         'username' => $_ENV['DB_USERNAME'] ?? 'root',
         'password' => $_ENV['DB_PASSWORD'] ?? '',
