714cfc0d348e02305ea3b58e1d4247d554b191a1
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2026-02-06 01:18:56 +0800

    nuked: uuid util simple call

--- src/lib/user.php
+++ src/lib/user.php
@@ -1,6 +1,13 @@
 <?php
 
 use Mindtrack\Server\Db\Users;
+use Ramsey\Uuid\Uuid;
+
+function uuid()
+{
+    $my_uuid = Uuid::uuid4();
+    return $my_uuid->toString();
+}
 
 function userData($uuid = null)
 {
